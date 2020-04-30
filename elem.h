436fa7670a50d3b330d18d08f1be6c37782189b0
Kevin Ramirez Mendez <42345834+kevin-ramen@users.noreply.github.com>
2020-04-30 07:16:30 -0500

    Add files via upload

---
+++ elem.h
@@ -0,0 +1,9 @@
+typedef int Elem;
+
+void ImpElemF(Elem e,FILE *out){fprintf(out,"%d\n",e);}
+
+void ImpElem(Elem e){ImpElemF(e,stdout);}
+
+int EsMenor(Elem x,Elem y){return x<y;}
+
+int EsIgual(Elem x, Elem y){return x==y;}
