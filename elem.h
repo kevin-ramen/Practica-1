3ad9ce13e7a0f59a5918947bd9f589b9dc18227e
Kevin Ramirez Mendez <42345834+kevin-ramen@users.noreply.github.com>
2020-04-30 07:15:34 -0500

    Delete elem.h

--- elem.h
+++
@@ -1,9 +0,0 @@
-typedef int Elem;
-
-void ImpElemF(Elem e,FILE *out){fprintf(out,"%d\n",e);}
-
-void ImpElem(Elem e){ImpElemF(e,stdout);}
-
-int EsMenor(Elem x,Elem y){return x<y;}
-
-int EsIgual(Elem x, Elem y){return x==y;}
